79d7ce4ef92becb0ccf5e0224f2ac8425c48f65d
Elkorm98 <elkorm13898@gmail.com>
2021-06-11 18:09:16 +0200

    data_frame

--- mainWindow.py
+++ mainWindow.py
@@ -1,18 +1,50 @@
-import tkinter as tk
-import pandas as pd 
-import moduls
+from tkinter import *
+import pandas as pd
+from tkinter import ttk, filedialog
+from moduls import file_open
 
-def main():
-    window = tk.Tk()
-    menu = tk.Menu(window)
-    window.config(menu=menu)
-    window.title("Aplikacja do analizy baz danych")
-    window.geometry("900x750")
-    file_menu = tk.Menu(menu)
-    menu.add_cascade(label = "File", menu = file_menu) 
-    file_menu.add_command(label= "Load",command = moduls.load_file)
-    window.mainloop()
+#Podstawowe ustawienia
+root = Tk()
+root.title('Program do analizy baz danych')
+root.geometry("700x500")
+
+#Miejsce do wyświetlania danych
+data_frame = Frame(root)
+data_frame.pack(pady=20)
 
 
-if __name__ == '__main__':
-    main()
+#Dodanie Scroolbar'ów
+tree_scrollY = Scrollbar(data_frame)
+tree_scrollX = Scrollbar(data_frame,orient = 'horizontal')
+data_tree = ttk.Treeview(data_frame,xscrollcommand = tree_scrollX.set, yscrollcommand = tree_scrollY.set)
+tree_scrollY.config(command=data_tree.yview)
+tree_scrollX.config(command=data_tree.xview)
+
+
+#Stworzenie Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+
+error_label = Label(root, text='')
+error_label.pack(pady=20)
+
+
+#Stworzenie Menu "File"
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = tree_scrollY, tree_scrollX = tree_scrollX))
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
--- moduls.py
+++ moduls.py
@@ -1,9 +1,40 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 import pandas as pd 
 
 
-def load_file():
-    file = filedialog.askopenfilename(filetypes= [("csv","*.csv")]) 
-    df = pd.read_csv(file)
-    print(df.head())
+# Funkcja otiwerająca plik
+def file_open(error_label,data_tree,tree_scrollX,tree_scrollY):
+	filename = filedialog.askopenfilename(
+		initialdir="C:/gui/",
+		title = "Open A File",
+		filetype=(("csv files", "*.csv"), ("All Files", "*.*"))
+		)
+
+	if filename:
+		try:
+			filename = r"{}".format(filename)
+			df = pd.read_csv(filename)
+		except ValueError:
+			error_label.config(text="Nie udało się otworzyć pliku!")
+		except FileNotFoundError:
+			error_label.config(text="PLik nie istnieje!")
+
+	data_tree.delete(*data_tree.get_children())
+
+	data_tree["column"] = list(df.columns)
+	data_tree["show"] = "headings"
+
+	for column in data_tree["column"]:
+		data_tree.heading(column, text=column)
+
+
+	df_rows = df.to_numpy().tolist()
+	for row in df_rows:
+		data_tree.insert("", "end", values=row)
+
+
+	tree_scrollX.pack(side=BOTTOM, fill=X)
+	tree_scrollY.pack(side=RIGHT, fill=Y)
+	data_tree.pack()
