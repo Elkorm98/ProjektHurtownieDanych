1a9c956ffa375bcdbaa58a3a1f9808dc8b321e4b
Elkorm98 <elkorm13898@gmail.com>
2021-06-12 20:57:53 +0200

    troche się zrobiło

--- mainWindow.py
+++ mainWindow.py
@@ -2,29 +2,50 @@ from tkinter import *
 from numpy.core.fromnumeric import size
 import pandas as pd
 from tkinter import ttk, filedialog
-from moduls import file_open
+from moduls import file_open, standard_stat , draw_hist, chose_scatplot_2
+import seaborn as sns
+import plotly.io as pio
 
 #Podstawowe ustawienia
 root = Tk()
 root.title('Program do analizy baz danych')
 root.geometry("700x500")
-#
-title_label = Label(root,width="70", height= "1",text = '')
-title_label.pack(padx=20)
+#Tytuł wyświetlanego pliku
+title_frame = Frame(root)
+title_frame.pack(side=TOP)
+
+title_label = Label(title_frame,text = '')
+title_label.pack(side = LEFT)
 
 
 #Miejsce do wyświetlania danych
 data_frame = Frame(root)
 data_frame.pack(pady=20)
 
+#Miejsce do wyświetlania statystyk
+stat_frame = Frame(root)
+stat_frame.pack(side =LEFT)
+
+#Mejsce do wyświetlania wykresów
+plot_frame = Frame(root)
+plot_frame.pack(side =RIGHT)
+tabControl = ttk.Notebook(plot_frame)
+tabs = []
 
 #Dodanie Scroolbar'ów
-tree_scrollY = Scrollbar(data_frame)
-tree_scrollX = Scrollbar(data_frame,orient = 'horizontal')
-data_tree = ttk.Treeview(data_frame,xscrollcommand = tree_scrollX.set, yscrollcommand = tree_scrollY.set)
-tree_scrollY.config(command=data_tree.yview)
-tree_scrollX.config(command=data_tree.xview)
+data_tree_scrollY = Scrollbar(data_frame)
+data_tree_scrollX = Scrollbar(data_frame,orient = 'horizontal')
+data_tree = ttk.Treeview(data_frame, xscrollcommand = data_tree_scrollX.set, yscrollcommand = data_tree_scrollY.set)
+data_tree_scrollY.config(command=data_tree.yview)
+data_tree_scrollX.config(command=data_tree.xview)
 
+
+#Statystyki
+stat_tree_scrollY = Scrollbar(stat_frame)
+stat_tree_scrollX = Scrollbar(stat_frame,orient = 'horizontal')
+stat_tree = ttk.Treeview(stat_frame, xscrollcommand = stat_tree_scrollX.set, yscrollcommand = stat_tree_scrollY.set)
+stat_tree_scrollY.config(command=stat_tree.yview)
+stat_tree_scrollX.config(command=stat_tree.xview)
 
 #Stworzenie Menu
 my_menu = Menu(root)
@@ -38,10 +59,22 @@ error_label.pack(pady  =20)
 #Stworzenie Menu "File"
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = tree_scrollY, tree_scrollX = tree_scrollX, title_label = title_label))
+file_menu.add_command(label="Open", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = data_tree_scrollY, tree_scrollX = data_tree_scrollX, title_label = title_label))
 
+#Stworzenie Menu "Podstawowe statystyki"
+stat_menu = Menu(my_menu, tearoff = False)
+my_menu.add_cascade(label = "Statystyki", menu = stat_menu)
+stat_menu.add_command(label="Podstawowe statystyki", command = lambda : standard_stat(stat_tree = stat_tree,tree_scrollY = stat_tree_scrollY, tree_scrollX = stat_tree_scrollX))
 
+#Stworzenie Menu "Hisotgramy"
+plot_menu = Menu(my_menu, tearoff = False)
+my_menu.add_cascade(label = "Wykresy", menu = plot_menu)
+plot_menu.add_command(label="Histogram", command = lambda : draw_hist(tabControl = tabControl) )
 
+scat_plot_menu=Menu(plot_menu, tearoff = False)
+plot_menu.add_cascade(label = "Wykres zależności", menu = scat_plot_menu)
+scat_plot_menu.add_command(label="dla 2 zmiennych",command = lambda : chose_scatplot_2(tabControl = tabControl))
+scat_plot_menu.add_command(label="dla 3 zmiennych")
 
 
 
--- moduls.py
+++ moduls.py
@@ -1,11 +1,22 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from tkinter import ttk
 import pandas as pd 
 import os
+import seaborn as sns
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import plotly.express as px
+from tkinter import simpledialog
+import plotly.io as pio
 
+
+df = pd.DataFrame()
 # Funkcja otiwerająca plik
 def file_open(error_label, data_tree, tree_scrollX, tree_scrollY, title_label):
+	global df
 	filename = filedialog.askopenfilename(
 		initialdir="C:/gui/",
 		title = "Open A File",
@@ -40,3 +51,78 @@ def file_open(error_label, data_tree, tree_scrollX, tree_scrollY, title_label):
 	title_label.config(text = os.path.basename(filename))
 	data_tree.pack()
 	#return df
+
+
+def standard_stat(stat_tree,tree_scrollX, tree_scrollY):
+	global df
+	if df.empty:
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+	else:
+		d = df.describe()
+		d.insert(0,"Podstawowe statystyki",d.index,True)
+		stat_tree.delete(*stat_tree.get_children())
+
+		stat_tree["column"] = list(d.columns)
+
+		stat_tree["show"] = "headings"
+		for column in stat_tree["column"]:
+			stat_tree.heading(column, text=column)
+
+		d_rows = d.to_numpy().tolist()
+
+		for row in d_rows:
+			stat_tree.insert("", "end", values=row)
+		
+
+		tree_scrollX.pack(side=BOTTOM, fill=X)
+		tree_scrollY.pack(side=RIGHT, fill=Y)
+		stat_tree.pack()
+
+
+def draw_hist(tabControl):
+	global df
+	if df.empty:
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+	else:
+		for i in df.columns:
+			tab = ttk.Frame(tabControl)
+			figure = Figure(figsize=(6, 6))
+			ax = figure.subplots()
+			hist = sns.histplot(df[i],ax = ax)
+			canvas = FigureCanvasTkAgg(figure, master=tab)
+			canvas.draw()
+			canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+			tabControl.add(tab, text = i)
+			tabControl.pack(expand = 1, fill ="both")
+
+
+def draw_scatplot_2(tabControl,var_1,var_2):
+	tab = ttk.Frame(tabControl)
+	figure = Figure(figsize=(6, 6))
+	ax = figure.subplots()
+	scat = px.scatter(x=df[var_1],y=df[var_2])
+	scat.show(renderer="iframe")
+	#canvas = FigureCanvasTkAgg(figure, master=tab)
+	#canvas.draw()
+	#canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+	#tabControl.add(tab, text = i)
+	#tabControl.pack(expand = 1, fill ="both")
+	
+
+def chose_scatplot_2(tabControl):
+	global df
+	window = Toplevel()
+	value = []
+	for column in list(df.columns):
+		value.append(column)
+	label_1 = Label(window, text ="Wybierz pierwszą zmienną" )
+	label_1.pack()
+	Lista_1 = ttk.Combobox(window, values = value)
+	Lista_1.pack()
+	label_2 = Label(window, text ="Wybierz drugą zmienną" )
+	label_2.pack()
+	Lista_2 = ttk.Combobox(window, values = value)
+	Lista_2.pack()
+	button = Button(window,  text= "Ok", command= lambda: draw_scatplot_2(tabControl= tabControl,var_1 = Lista_1.get(), var_2 = Lista_2.get()))
+	button.pack()
+		
