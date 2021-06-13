3096edcb0c47246b34f11061680ac88f04d8f854
Elkorm98 <elkorm13898@gmail.com>
2021-06-13 22:11:43 +0200

    iteracja 2

--- mainWindow.py
+++ mainWindow.py
@@ -2,7 +2,7 @@ from tkinter import *
 from numpy.core.fromnumeric import size
 import pandas as pd
 from tkinter import ttk, filedialog
-from moduls import file_open, standard_stat , draw_hist, chose_scatplot_2, chose_scatplot_3
+from moduls import file_open, standard_stat , draw_hist, chose_scatplot_2, chose_scatplot_3, chose_grupowanie
 import seaborn as sns
 import plotly.io as pio
 
@@ -20,16 +20,17 @@ title_label.pack(side = LEFT)
 
 #Miejsce do wyświetlania danych
 data_frame = Frame(root)
-data_frame.pack(pady=20)
+data_frame.pack()
 
 #Miejsce do wyświetlania statystyk
-stat_frame = Frame(root)
-stat_frame.pack(side =LEFT)
+stat_frame = Frame(root,maxsize=300)
+stat_frame.pack(side = LEFT)
+
 
 #Mejsce do wyświetlania wykresów
-plot_frame = Frame(root)
-plot_frame.pack(side =RIGHT)
-tabControl = ttk.Notebook(plot_frame)
+plot_frame = Frame(root,  maxsize=300)
+plot_frame.pack(side = RIGHT)
+plot_tabControl = ttk.Notebook(plot_frame)
 tabs = []
 
 #Dodanie Scroolbar'ów
@@ -46,7 +47,7 @@ stat_tree_scrollX = Scrollbar(stat_frame,orient = 'horizontal')
 stat_tree = ttk.Treeview(stat_frame, xscrollcommand = stat_tree_scrollX.set, yscrollcommand = stat_tree_scrollY.set)
 stat_tree_scrollY.config(command=stat_tree.yview)
 stat_tree_scrollX.config(command=stat_tree.xview)
-
+stat_tabControl = ttk.Notebook(stat_frame)
 #Stworzenie Menu
 my_menu = Menu(root)
 root.config(menu=my_menu)
@@ -58,24 +59,28 @@ error_label.pack(pady  =20)
 
 #Stworzenie Menu "File"
 file_menu = Menu(my_menu, tearoff=False)
-my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = data_tree_scrollY, tree_scrollX = data_tree_scrollX, title_label = title_label))
+my_menu.add_cascade(label="PLik", menu=file_menu)
+file_menu.add_command(label="Otwórz plik", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = data_tree_scrollY, tree_scrollX = data_tree_scrollX, title_label = title_label))
 
 #Stworzenie Menu "Podstawowe statystyki"
 stat_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label = "Statystyki", menu = stat_menu)
-stat_menu.add_command(label="Podstawowe statystyki", command = lambda : standard_stat(stat_tree = stat_tree,tree_scrollY = stat_tree_scrollY, tree_scrollX = stat_tree_scrollX))
+stat_menu.add_command(label="Podstawowe statystyki", command = lambda : standard_stat(stat_tree = stat_tree,tree_scrollY = stat_tree_scrollY, tree_scrollX = stat_tree_scrollX, tabControl = stat_tabControl))
 
-#Stworzenie Menu "Hisotgramy"
+#Stworzenie Menu "Wykresy"
 plot_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label = "Wykresy", menu = plot_menu)
-plot_menu.add_command(label="Histogram", command = lambda : draw_hist(tabControl = tabControl) )
+plot_menu.add_command(label="Histogram", command = lambda : draw_hist(tabControl = plot_tabControl) )
 
 scat_plot_menu=Menu(plot_menu, tearoff = False)
 plot_menu.add_cascade(label = "Wykres zależności", menu = scat_plot_menu)
-scat_plot_menu.add_command(label="dla 2 zmiennych",command = lambda : chose_scatplot_2(tabControl = tabControl))
-scat_plot_menu.add_command(label="dla 3 zmiennych",command = lambda : chose_scatplot_3(tabControl = tabControl))
+scat_plot_menu.add_command(label="dla 2 zmiennych",command = lambda : chose_scatplot_2(tabControl = plot_tabControl))
+scat_plot_menu.add_command(label="dla 3 zmiennych",command = lambda : chose_scatplot_3(tabControl = plot_tabControl))
 
+#Stworzenie Menu "Grupowanie"
+group_menu = Menu(my_menu, tearoff =  False)
+my_menu.add_cascade(label = "Grupowanie", menu = group_menu)
+group_menu.add_command(label = "Grupuj", command = lambda : chose_grupowanie(tabControl = stat_tabControl))
 
 
 
--- moduls.py
+++ moduls.py
@@ -6,7 +6,7 @@ import pandas as pd
 import os
 import seaborn as sns
 from matplotlib.backend_bases import key_press_handler
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 import plotly.express as px
 from tkinter import simpledialog
@@ -18,6 +18,9 @@ import matplotlib.pyplot as plt
 df = pd.DataFrame()
 # Funkcja otiwerająca plik
 def file_open(error_label, data_tree, tree_scrollX, tree_scrollY, title_label):
+	'''
+	Ta funkcja otwiera plik 
+	'''
 	global df
 	filename = filedialog.askopenfilename(
 		initialdir="C:/gui/",
@@ -52,10 +55,10 @@ def file_open(error_label, data_tree, tree_scrollX, tree_scrollY, title_label):
 	tree_scrollY.pack(side=RIGHT, fill=Y)
 	title_label.config(text = os.path.basename(filename))
 	data_tree.pack()
-	#return df
 
 
-def standard_stat(stat_tree,tree_scrollX, tree_scrollY):
+
+def standard_stat(stat_tree,tree_scrollX, tree_scrollY, tabControl):
 	global df
 	if df.empty:
 		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
@@ -79,6 +82,9 @@ def standard_stat(stat_tree,tree_scrollX, tree_scrollY):
 		tree_scrollX.pack(side=BOTTOM, fill=X)
 		tree_scrollY.pack(side=RIGHT, fill=Y)
 		stat_tree.pack()
+		tabControl.add(stat_tree, text = "Podstawowe statystyki")
+		tabControl.pack(expand = 1, fill ="both")
+
 
 
 def draw_hist(tabControl):
@@ -115,14 +121,17 @@ def draw_scatplot_2(tabControl,var_1,var_2,window):
 def draw_scatplot_3(tabControl,var_1,var_2,var_3,window):
 	global df
 	tab = ttk.Frame(tabControl)
-	figure = plt.figure()
-	ax = figure.add_subplot(111, projection = "3d")
-	scat = sns.scatter(data = df, x = var_1, y = var_2,z = var_3, ax = ax)
+	figure = Figure(figsize=(5, 4), dpi=100)
+	ax = figure.add_subplot(111, projection="3d")
+	ax.scatter(df[var_1], df[var_2], df[var_3])
+	
 	canvas = FigureCanvasTkAgg(figure, master=tab)
 	canvas.draw()
 	canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+	toolbar = NavigationToolbar2Tk(canvas, tab)
 	tabControl.add(tab, text = "New Scater")
 	tabControl.pack(expand = 1, fill ="both")
+	toolbar.update()
 	window.destroy()
 
 	
@@ -163,6 +172,69 @@ def chose_scatplot_3(tabControl):
 	label_3.pack()
 	Lista_3 = ttk.Combobox(window, values = value)
 	Lista_3.pack()
-	button = Button(window,  text= "Ok", command= lambda: draw_scatplot_3(tabControl= tabControl,var_1 = Lista_1.get(), var_2 = Lista_2.get(), var_3 = Lista_3.get,window = window))
+	button = Button(window,  text= "Ok", command= lambda: draw_scatplot_3(tabControl= tabControl,var_1 = Lista_1.get(), var_2 = Lista_2.get(), var_3 = Lista_3.get(),window = window))
 	button.pack()
-		
+
+
+def grupowanie(tabControl,var_wyswietlana, var_grupowana, window):
+	global df
+	tab = ttk.Frame(tabControl)
+	stat_tree = ttk.Treeview(tab)
+	d = df.groupby(var_grupowana).describe()[var_wyswietlana]
+	d.insert(0,"Grupa",d.index,True)
+	stat_tree.delete(*stat_tree.get_children())
+
+	stat_tree["column"] = list(d.columns)
+
+	stat_tree["show"] = "headings"
+	for column in stat_tree["column"]:
+		stat_tree.heading(column, text=column)
+
+	d_rows = d.to_numpy().tolist()
+
+	for row in d_rows:
+		stat_tree.insert("", "end", values=row)
+	stat_tree.pack()
+	tabControl.add(tab, text = var_grupowana + " x " + var_wyswietlana)
+	tabControl.pack(expand = 1, fill ="both")
+	window.destroy()
+
+
+def chose_wyswietlana(var_1,window,tabControl):
+	window.destroy()
+	global df
+	window = Toplevel()
+	value = []
+	for column in list(df.columns):
+		value.append(column)
+	value.remove(var_1)
+	label_1 = Label(window, text ="Wybierz zmienną do grupowania" )
+	label_1.pack()
+	Lista_1 = ttk.Combobox(window, values = value)
+	Lista_1.pack()
+	button = Button(window,  text = "Ok", command= lambda: grupowanie(tabControl= tabControl,var_wyswietlana = Lista_1.get(),var_grupowana = var_1, window = window))
+	button.pack()
+
+
+def sprawdz(var_1, window, tabControl):
+	global df
+	x = df[var_1].unique().size
+	if x <= 10:
+		chose_wyswietlana(var_1 = var_1, window = window, tabControl = tabControl)
+	else:
+		messagebox.showerror("Zbyt duża liczba grup", "Zbyt duża liczba grup (max : 10)")
+		window.destroy()
+
+
+def chose_grupowanie(tabControl):
+	global df
+	window = Toplevel()
+	value = []
+	for column in list(df.columns):
+		value.append(column)
+	label_1 = Label(window, text ="Wybierz zmienną do grupowania" )
+	label_1.pack()
+	Lista_1 = ttk.Combobox(window, values = value)
+	Lista_1.pack()
+	button = Button(window,  text = "Ok", command = lambda : sprawdz(var_1 = Lista_1.get(), window = window, tabControl= tabControl))
+	button.pack()
