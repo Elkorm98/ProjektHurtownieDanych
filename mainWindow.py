83d4e2a089725174e567fb030f029c925daee79f
Elkorm98 <elkorm13898@gmail.com>
2021-06-14 21:30:45 +0200

    iteracja 4

--- mainWindow.py
+++ mainWindow.py
@@ -2,7 +2,8 @@ from tkinter import *
 from numpy.core.fromnumeric import size
 import pandas as pd
 from tkinter import ttk, filedialog
-from moduls import file_open, standard_stat , chose_hist, chose_scatplot_2, chose_scatplot_3, chose_grupowanie
+
+from moduls import file_open, standard_stat , chose_hist, chose_scatplot_2, chose_scatplot_3, chose_grupowanie, connection_open, cofnij, chose_filtr, chose_kolumne
 import seaborn as sns
 
 #Podstawowe ustawienia
@@ -56,6 +57,7 @@ error_label.pack(pady  =20)
 file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label="Plik", menu=file_menu)
 file_menu.add_command(label="Otwórz plik", command=lambda : file_open(error_label=error_label,data_tree=data_tree,tree_scrollY = data_tree_scrollY, tree_scrollX = data_tree_scrollX, title_label = title_label))
+file_menu.add_command(label = "Cofnij", command = lambda : cofnij(data_tree = data_tree))
 file_menu.add_command(label = "Wyjdź", command = lambda : root.destroy())
 #Stworzenie Menu "Podstawowe statystyki"
 stat_menu = Menu(my_menu, tearoff = False)
@@ -76,6 +78,22 @@ scat_plot_menu.add_command(label="dla 3 zmiennych",command = lambda : chose_scat
 group_menu = Menu(my_menu, tearoff =  False)
 my_menu.add_cascade(label = "Grupowanie", menu = group_menu)
 group_menu.add_command(label = "Grupuj", command = lambda : chose_grupowanie(tabControl = stat_tabControl, stat_frame=stat_frame))
+
+#Łączenie tabeli
+laczenie_menu = Menu(my_menu, tearoff =  False)
+my_menu.add_cascade(label = "Łączenie", menu = laczenie_menu)
+laczenie_menu.add_command(label = "Połącz tabele", command = lambda :connection_open(data_tree= data_tree))
+
+#Filtrowanie
+filtr_menu = Menu(my_menu, tearoff =  False)
+my_menu.add_cascade(label = "Filtry", menu = filtr_menu)
+filtr_menu.add_command(label = "Filtrowanie", command = lambda :chose_filtr(data_tree= data_tree))
+
+#Dodaj kolumne
+kolumna_menu = Menu(my_menu, tearoff =  False)
+my_menu.add_cascade(label = "Kolumny", menu = kolumna_menu)
+kolumna_menu.add_command(label = "Dodaj kolumne", command = lambda :chose_kolumne(data_tree= data_tree))
+
 
 
 
--- moduls.py
+++ moduls.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 
 
 df = pd.DataFrame()
+df_list = []
+
 # Funkcja otiwerająca plik
 def file_open(error_label, data_tree, tree_scrollX, tree_scrollY, title_label):
 	'''
@@ -99,7 +101,7 @@ def draw_hist(tabControl,window, var):
 	tab = ttk.Frame(tabControl)
 	figure = Figure(figsize=(10, 10))
 	ax = figure.subplots()
-	hist = sns.histplot(df[var],ax = ax)
+	hist = sns.histplot(df[var], ax = ax)
 	canvas = FigureCanvasTkAgg(figure, master=tab)
 	canvas.draw()
 	canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
@@ -110,7 +112,7 @@ def draw_hist(tabControl,window, var):
 def chose_hist(tabControl):
 	global df
 	if df.empty:
-    		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
 	else:
 		window = Toplevel()
 		value = []
@@ -165,7 +167,7 @@ def draw_scatplot_3(tabControl,var_1,var_2,var_3,window):
 def chose_scatplot_2(tabControl):
 	global df
 	if df.empty:
-    		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
 	else:
 		window = Toplevel()
 		value = []
@@ -186,7 +188,7 @@ def chose_scatplot_2(tabControl):
 def chose_scatplot_3(tabControl):
 	global df
 	if df.empty:
-    		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
 	else:
 		window = Toplevel()
 		value = []
@@ -267,7 +269,7 @@ def sprawdz(stat_frame,var_1, window, tabControl):
 def chose_grupowanie(tabControl,stat_frame):
 	global df
 	if df.empty:
-    		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
 	else:
 		window = Toplevel()
 		value = []
@@ -279,3 +281,240 @@ def chose_grupowanie(tabControl,stat_frame):
 		Lista_1.pack()
 		button = Button(window,  text = "Ok", command = lambda : sprawdz(var_1 = Lista_1.get(), window = window, tabControl= tabControl, stat_frame= stat_frame))
 		button.pack()
+
+
+def connection_open(data_tree):
+	global df
+	
+	if df.empty:
+		messagebox.showerror("Brak Danych", "Brakuje do analizy!")
+	else:
+		filename = filedialog.askopenfilename(
+			initialdir="C:/gui/",
+			title = "Open A File",
+			filetype=(("csv files", "*.csv"), ("All Files", "*.*"))
+			)
+
+		if filename:
+			try:
+				filename = r"{}".format(filename)
+				df_connect = pd.read_csv(filename)
+			except ValueError:
+				error_label.config(text="Nie udało się otworzyć pliku!")
+			except FileNotFoundError:
+				error_label.config(text="PLik nie istnieje!")
+
+
+			window = Toplevel()
+			value_1 = []
+			value_2 = []
+			for column in list(df.columns):
+					value_1.append(column)
+			for column in list(df_connect.columns):
+				value_2.append(column)
+			
+			label_1 = Label(window, text ="Wybierz kolumnę tabeli podstawowej do dokonania złączenia" )
+			label_1.pack()
+			Lista_1 = ttk.Combobox(window, values = value_1)
+			Lista_1.pack()
+
+			label_2 = Label(window, text ="Wybierz kolumnę tabeli wczytanej do dokonania złączenia" )
+			label_2.pack()
+			Lista_2 = ttk.Combobox(window, values = value_2)
+			Lista_2.pack()
+
+			button = Button(window,  text = "Ok", command = lambda : connecting(var_1 = Lista_1.get(), var_2 = Lista_2.get(), window = window, df_connect =df_connect, data_tree = data_tree))
+			button.pack()
+
+
+def connecting(var_1, var_2, window, df_connect, data_tree):
+	global df
+	global df_list
+	window.destroy()
+	df_list.append(df)
+
+	df = pd.merge(df,df_connect, left_on = var_1, right_on = var_2)
+
+	data_tree.delete(*data_tree.get_children())
+
+	data_tree["column"] = list(df.columns)
+	data_tree["show"] = "headings"
+
+	for column in data_tree["column"]:
+		data_tree.heading(column, text=column)
+
+
+	df_rows = df.to_numpy().tolist()
+	for row in df_rows:
+		data_tree.insert("", "end", values=row)
+
+
+	tree_scrollX.pack(side=BOTTOM, fill=X)
+	tree_scrollY.pack(side=RIGHT, fill=Y)
+	title_label.config(text = os.path.basename(filename))
+	data_tree.pack()
+
+
+def cofnij(data_tree):
+	global df
+	global df_list
+	if not len(df_list):
+		messagebox.showerror("Brak porzednich stanów tabeli", "Brak porzednich stanów tabeli!")
+	else:
+		df = df_list[-1]
+		df_list.pop(-1)
+		data_tree.delete(*data_tree.get_children())
+
+	data_tree["column"] = list(df.columns)
+	data_tree["show"] = "headings"
+
+	for column in data_tree["column"]:
+		data_tree.heading(column, text=column)
+
+
+	df_rows = df.to_numpy().tolist()
+	for row in df_rows:
+		data_tree.insert("", "end", values=row)
+
+
+	data_tree.pack()
+
+
+def chose_filtr(data_tree):
+	global df
+	if df.empty:
+		messagebox.showerror("Brak wczytanych danych", "Brak wczytanych danych!")
+	else:
+		window = Toplevel()
+		value = []
+		for column in list(df.columns):
+			value.append(column)
+
+		label_1 = Label(window, text ="Wybierz zmienną na której ma zadziałać filtr" )
+		label_1.pack()
+		Lista_1 = ttk.Combobox(window, values = value)
+		Lista_1.pack()
+		entry = Text(window, width = 5, height = 1)
+		entry.pack()
+		button = Button(window,  text = "Ok", command = lambda : filtrowanie(var = Lista_1.get(), val = entry.get("1.0","end-1c"), window = window,data_tree = data_tree))
+		button.pack()
+
+def filtrowanie(var, val, window, data_tree):
+	global df_list
+	global df
+	window.destroy()
+	df_list.append(df)
+	
+	try:
+		df = df[df[var] == int(val)]
+	except:
+		df = df[df[var] == val]
+	data_tree.delete(*data_tree.get_children())
+
+	data_tree["column"] = list(df.columns)
+	data_tree["show"] = "headings"
+
+	for column in data_tree["column"]:
+		data_tree.heading(column, text=column)
+
+
+	df_rows = df.to_numpy().tolist()
+	for row in df_rows:
+		data_tree.insert("", "end", values=row)
+
+	data_tree.pack()
+
+
+def chose_kolumne(data_tree):
+	global df
+	if df.empty:
+		messagebox.showerror("Brak wczytanych danych", "Brak wczytanych danych!")
+	else:
+		dzialania = ["+","-","*"]
+		typy = ["Integer", "Float"]
+		window = Toplevel()
+		value = []
+		for column in list(df.columns):
+			value.append(column)
+
+		label_7 = Label(window, text ="Wybierz nazwę nowej kolumny")
+		label_7.pack()
+		entry_7 = Text(window,width = 5, height = 1)
+		entry_7.pack()
+
+		label_1 = Label(window, text ="Wybierz skalar do pierwszej zmmiennej" )
+		label_1.pack()
+		entry_1 = Text(window,width = 5, height = 1)
+		entry_1.pack()
+
+		label_2 = Label(window, text ="Wybierz pierwszą zmienną" )
+		label_2.pack()
+		Lista_1 = ttk.Combobox(window, values = value)
+		Lista_1.pack()
+
+		label_3 = Label(window, text ="Wybierz działanie" )
+		label_3.pack()
+		Lista_3 = ttk.Combobox(window, values = dzialania)
+		Lista_3.pack()
+
+		label_4 = Label(window, text ="Wybierz skalar do drugiej zmmiennej")
+		label_4.pack()
+		entry_4 = Text(window,width = 5, height = 1)
+		entry_4.pack()
+
+		label_5 = Label(window, text ="Wybierz drugą zmienną" )
+		label_5.pack()
+		Lista_5 = ttk.Combobox(window, values = value)
+		Lista_5.pack()
+
+		label_6 = Label(window, text ="Wybierz wyraz wolny")
+		label_6.pack()
+		entry_6 = Text(window,width = 5, height = 1)
+		entry_6.pack()
+
+		label_8 = Label(window, text ="Wybierz typ kolumny" )
+		label_8.pack()
+		Lista_8 = ttk.Combobox(window, values = typy)
+		Lista_8.pack()
+
+		button = Button(window,  text = "Ok", command = lambda : dodaj_kolumne(tytul =entry_7.get("1.0","end-1c"),
+		skalar_1 = entry_1.get("1.0","end-1c"), zmienna_1 = Lista_1.get(), dzialania = Lista_3.get(), skalar_2 = entry_4.get("1.0","end-1c"),
+		zmienna_2 = Lista_5.get(), wyraz_wolny = entry_6.get("1.0","end-1c"),typy = Lista_8.get(),
+		window = window, data_tree = data_tree))
+		button.pack()
+
+
+def dodaj_kolumne(tytul, skalar_1, zmienna_1, dzialania, skalar_2, zmienna_2, wyraz_wolny, typy, window, data_tree):
+	global df
+	global df_list
+	window.destroy()
+	df_list.append(df)
+
+	if typy == "Float":
+		df[tytul] = df.apply(
+		lambda x:
+		x[zmienna_1] * float(skalar_1) + x[zmienna_2] * float(skalar_2) + float(wyraz_wolny) if dzialania =="+" else 
+		x[zmienna_1] * float(skalar_1) - x[zmienna_2] * float(skalar_2) + float(wyraz_wolny) if dzialania =="-" else 
+		x[zmienna_1] * float(skalar_1) * x[zmienna_2] * float(skalar_2) + float(wyraz_wolny) if dzialania =="*" else 
+		np.nan, axis = 1)
+	elif typy == "Integer":
+		df[tytul] = df.apply(
+		lambda x:
+		int(x[zmienna_1] * float(skalar_1) + x[zmienna_2] * float(skalar_2) + float(wyraz_wolny)) if dzialania =="+" else 
+		int(x[zmienna_1] * float(skalar_1) - x[zmienna_2] * float(skalar_2) + float(wyraz_wolny)) if dzialania =="-" else 
+		int(x[zmienna_1] * float(skalar_1) * x[zmienna_2] * float(skalar_2) + float(wyraz_wolny)) if dzialania =="*" else	
+		np.nan, axis = 1)
+	data_tree.delete(*data_tree.get_children())
+
+	data_tree["column"] = list(df.columns)
+	data_tree["show"] = "headings"
+
+	for column in data_tree["column"]:
+		data_tree.heading(column, text=column)
+
+
+	df_rows = df.to_numpy().tolist()
+	for row in df_rows:
+		data_tree.insert("", "end", values=row)
+
+	data_tree.pack()
